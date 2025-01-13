361284574565a8359b5ce76775601fddd522cbc4
cerh2801 <cerh2802@gmail.com>
2025-01-13 16:23:15 -0500

    septoversion

---
+++ house.py
@@ -0,0 +1,10 @@
+name = input('Cual es tu nombre?\n')
+print(name.capitalize())
+
+if name =='Harry' or name=='Hermanone' or name=='Ron':
+    print('Gryffindor')
+elif name =='Draco':
+    print('Slytherin')
+else:
+    print('Who?')
+   
